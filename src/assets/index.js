f4b2c8f4b06ab418708c1671103703928f11e94f
kjh0512 <103028143+kjh0512@users.noreply.github.com>
2024-05-08 15:26:27 +0900

    헤더생성

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as MSJLogo } from "./img/msjLogo.png";
+export { default as LogoutLogin } from "./img/LoginLogout.png";
+export { default as Hold } from "./img/hold.png";
+export { default as MyPage } from "./img/mypage.png";
+export { default as Participation } from "./img/Participation.png";
+export { default as Question } from "./img/question.png";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+import React from "react";
+import {
+  MSJLogo,
+  LogoutLogin,
+  Hold,
+  Participation,
+  MyPage,
+  Question,
+} from "../assets";
+const NavLinkWithIcon = ({ icon, children, to }) => (
+  <NavLink to={to}>
+    <img
+      src={icon}
+      alt=""
+      style={{ marginRight: "8px", verticalAlign: "middle" }}
+    />
+    {children}
+  </NavLink>
+);
 const Header = () => {
   return (
-    <div>어 이거 Header</div>
-  )
-}
+    <HeaderContainer>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Nav>
+        <NavLinkWithIcon to="/hold" icon={Hold}>
+          대회 대최
+        </NavLinkWithIcon>
+        <NavLinkWithIcon to="/participation" icon={Participation}>
+          대회 참여
+        </NavLinkWithIcon>
+        <NavLinkWithIcon to="/question" icon={Question}>
+          질문 게시판{" "}
+        </NavLinkWithIcon>
+      </Nav>
+      <Nav>
+        <NavLinkWithIcon to="/login" icon={LogoutLogin}>
+          로그인
+        </NavLinkWithIcon>
+        <NavLinkWithIcon to="/mypage" icon={MyPage}>
+          마이페이지
+        </NavLinkWithIcon>
+      </Nav>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
+
+const HeaderContainer = styled.header`
+  height: 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Logo = styled.h1`
+  width: 180px;
+  height: 72px;
+  font-size: 24px;
+  color: white;
+  background-image: url(${MSJLogo});
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+`;
+
+const NavLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  margin-left: 20px;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
