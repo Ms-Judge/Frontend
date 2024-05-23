ab0b8af2f46a9a4bc58aa3b58c63a33448042b48
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2024-05-23 17:16:13 +0900

    리스트 퍼블리싱

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled/macro';
 import Table from 'react-bootstrap/Table';
+import { Pagination } from 'react-bootstrap';
 
 // Table 컴포넌트를 styled-components로 감싸 스타일 정의
 const StyledTable = styled(Table)`
@@ -27,7 +28,7 @@ const StyledTable = styled(Table)`
           background-color: #B8FADD; // 행 호버 시 배경색 변경
         }
         td {
-
+          
         }
       }
     }
@@ -35,6 +36,17 @@ const StyledTable = styled(Table)`
 `;
 
 const List = () => {
+  const [active, setActive] = useState(1);
+
+  let items = [];
+  for (let number = 1; number <= 5; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === active} onClick={() => setActive(number)}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
   return (
     <ListWrapper>
       <TitleName>대회 목록</TitleName>
@@ -98,9 +110,9 @@ const List = () => {
             <td>2024년 5월 31일</td>
             <td>진행중</td>
           </tr>
-          {/* 나머지 행들 */}
         </tbody>
       </StyledTable>
+      <StyledPagination>{items}</StyledPagination>
     </ListWrapper>
   );
 }
@@ -119,3 +131,16 @@ const ListWrapper = styled.div`
   align-items: center; // 가로 축에서 중앙 정렬합니다.
   margin-top: 40px;
 `
+
+const StyledPagination = styled(Pagination)`
+  .page-item.active .page-link {
+    background-color: #B8FADD;
+    border-color: #B8FADD;
+  }
+  .page-link {
+    color: black;
+  }
+  .page-link:hover {
+    color: #B8FADD;
+  }
+`;
