8dd03096281c624f9cd744d995a93dc0da41c3c7
kjh0512 <103028143+kjh0512@users.noreply.github.com>
2024-05-24 10:23:51 +0900

    헤더 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,16 +10,15 @@ import {
   Question,
   ProblemUpload,
 } from "../assets";
+
+// NavLinkWithIcon 컴포넌트 수정
 const NavLinkWithIcon = ({ icon, children, to }) => (
   <NavLink to={to}>
-    <img
-      src={icon}
-      alt=""
-      style={{ marginRight: "8px", verticalAlign: "middle" }}
-    />
+    <Icon src={icon} alt="" />
     {children}
   </NavLink>
 );
+
 const Header = () => {
   return (
     <HeaderContainer>
@@ -62,6 +61,7 @@ const HeaderContainer = styled.header`
   padding: 0 20px;
   background-color: #ffffff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+ 
 `;
 
 const Logo = styled.h1`
@@ -77,6 +77,14 @@ const Nav = styled.nav`
   display: flex;
   align-items: center;
   font-size: 30px;
+
+  @media (max-width: 500px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 1024px) {
+    font-size: 20px;
+  }
 `;
 
 const NavLink = styled(Link)`
@@ -85,5 +93,19 @@ const NavLink = styled(Link)`
   margin-left: 20px;
   &:hover {
     text-decoration: underline;
+  }
+`;
+
+const Icon = styled.img`
+  margin-right: 8px;
+  vertical-align: middle;
+  width: 24px;
+
+  @media (max-width: 1024px) {
+    width: 30px;
+  }
+
+  @media (max-width: 500px) {
+    width: 16px;
   }
 `;
